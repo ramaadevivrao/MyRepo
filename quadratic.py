085aabb64f84b5a84d888e46f1449dd93a2b961f
ramaadevivrao <93392543+ramaadevivrao@users.noreply.github.com>
2021-10-29 20:26:13 +0530

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -10,4 +10,4 @@ d = (b**2) - (4*a*c)
 sol1 = (-b-cmath.sqrt(d))/(2*a)
 sol2 = (-b+cmath.sqrt(d))/(2*a)
 
-print('The solution are {0} and {1}'.format(sol1,sol2))
+print('The solution is {0} and {1}'.format(sol1,sol2))
